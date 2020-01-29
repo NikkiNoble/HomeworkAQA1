db5b1005208a8c618cb02ef382148359a075924d
Anastasia Motolova <motolovanastya@gmail.com>
2020-01-29 13:38:37 -0500

    Initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,29 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void ShouldAdviseToBuyMoreIfAmountIsLessThan1000() {
+        CashbackHackService cashback = new CashbackHackService();
+        int amount = 900;
+
+        int actual = cashback.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void ShouldNotAdviseToBuyMoreIfAmountIs1000() {
+        CashbackHackService cashback = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashback.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+}
